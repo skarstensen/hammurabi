5928093ac8f8520fc5b3445a053f3f0cc98865bb
Stephen Karstensen <lpstudiostandard@gmail.com>
2025-02-15 17:28:21 -0800

    Gameplay loop implemented, UI refactored

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,17 +11,17 @@ class_name Game
 @export var populationLabel:Label
 @export var grainLabel:Label
 @export var acresLabel:Label
-@export var yearCounterLabel:Label
 
-@export var quantitySpinbox:SpinBox
-
-@export var controlsContainer:PanelContainer
+@export var controlsContainer:ControlPanel
 @export var playButtonsContainer:PanelContainer
 
 @export_group("Game Parameters")
 @export var defaultReignLength:int = 10
 @export var startingPopulation:int = 95
 @export var startingAcres:int = 1000
+@export var grainRequiredPerPerson:int = 20
+@export var acresTendedPerPerson:int = 10
+@export var bushelsRequiredPerAcre:int = 2
 
 var population:int:
 	set(value):
@@ -41,7 +41,7 @@ var land:int:
 var year:int:
 	set(value):
 		year = value
-		yearCounterLabel.text = str(year)
+		controlsContainer.yearLabel.text = str(year)
 
 var reignLength:int
 var landPrice:int
@@ -52,7 +52,6 @@ var eatenByRats:int
 var harvestedPerAcre:int
 
 var acresPlantedThisYear:int
-var landOwnedModifer:int
 var grainFedToPeople:int
 
 func startGame():
@@ -76,13 +75,9 @@ func startGame():
 	population = startingPopulation
 	land = startingAcres
 	acresPlantedThisYear = land
+	grainFedToPeople = population * grainRequiredPerPerson
 	
 	processYear()
-
-
-func _on_menu_system_start_game(reign):
-	reignLength = reign
-	startGame()
 
 
 func displayLine(text:String):
@@ -92,11 +87,11 @@ func displayLine(text:String):
 		messageBox.text = "%s\n%s" % [messageBox.text, text]
 	
 	# Minor grammatical cleanup
-	if (messageBox.text.contains("1 bushels")):
-		messageBox.text.replace("1 bushels", "1 bushel")
+	if (messageBox.text.contains(" 1 bushels")):
+		messageBox.text = messageBox.text.replace("1 bushels", "1 bushel")
 	
-	if (messageBox.text.contains("1 people")):
-		messageBox.text.replace("1 people", "1 person")
+	if (messageBox.text.contains(" 1 people")):
+		messageBox.text = messageBox.text.replace("1 people", "1 person")
 
 
 func processYear():
@@ -114,19 +109,21 @@ func processYear():
 	
 	displayLine("The population is now %s." % population)
 	
-	displayLine("The city owns %s acres." % land)
-	displayLine("We've harvested %s bushels per acre." % harvestedPerAcre)
+	displayLine("The city owns %s acres of land." % land)
+	displayLine("We harvested %s bushels of grain per acre." % harvestedPerAcre)
 	
 	if (eatenByRats > 0):
 		displayLine("Rats ate %s bushels." % eatenByRats)
 		
-	displayLine("We now have %s in storage." % grain)
+	displayLine("We now have %s bushels in storage." % grain)
 	
 	displayLine("Land is trading at %s bushels per acre." % landPrice)
+	
+	controlsContainer.setBuySellMode()
 
 
 func processPopulationChange():
-	starved = max(0, population - (grainFedToPeople / 20))
+	starved = max(0, population - (grainFedToPeople / grainRequiredPerPerson))
 	diedFromPlague = 0
 	cameToCity = max(randi_range(1, 5) * (20 * land + grain) / population / 100, 1)
 	
@@ -170,8 +167,32 @@ func endReign():
 	playButtonsContainer.show()
 
 
-func _on_next_year_button_pressed():
-	advanceYear()
+func adjustLandOwnership(quantity:int):
+	land += quantity
+	grain -= quantity * landPrice
+
+	setFeedMode()
+
+
+func skipLandTrade():
+	displayLine("Very well.  We shall not trade for any land this year.")
+	setFeedMode()
+
+
+func setFeedMode():
+	displayLine("How many bushels of grain do you wish to feed our people?")
+	controlsContainer.setFeedMode()
+	
+
+func setPlantMode():
+	displayLine("How many acres of land do you wish to plant with seed?")
+	controlsContainer.setPlantMode()
+	
+	
+# Signal Handlers ##############################################################
+func _on_menu_system_start_game(reign):
+	reignLength = reign
+	startGame()
 
 
 func _on_play_again_button_pressed():
@@ -181,3 +202,75 @@ func _on_play_again_button_pressed():
 func _on_quit_to_title_button_pressed():
 	menuSystem.show()
 	gameUI.hide()
+
+
+func _on_buy_button_pressed():
+	var acresToPurchase:int = controlsContainer.quantity.value
+	var cost:int = acresToPurchase * landPrice
+	
+	if (acresToPurchase == 0):
+		skipLandTrade()
+	else:
+		if (cost <= grain):
+			displayLine("Very well.  We have purchased %s acres of land." % acresToPurchase)
+			adjustLandOwnership(acresToPurchase)
+		else:
+			displayLine("Hammurabi, that much land would cost us %s bushels of grain.  We only have %s available." % [cost, grain])
+
+
+func _on_sell_button_pressed():
+	var acresToSell:int = controlsContainer.quantity.value
+	
+	if (acresToSell == 0):
+		skipLandTrade()
+	else:
+		if (acresToSell > land):
+			displayLine("Hammurabi, you cannot sell that much land!  We only have %s acres." % land)
+		else:
+			displayLine("Very well.  We have sold %s acres of land." % acresToSell)
+			adjustLandOwnership(acresToSell)
+
+
+func _on_plant_button_pressed():
+	var acresToPlant:int = controlsContainer.quantity.value
+	var peopleNeeded:int = acresToPlant / acresTendedPerPerson
+	var grainNeeded:int = acresToPlant / bushelsRequiredPerAcre
+	var canPlant:bool = true
+
+	if (peopleNeeded > population):
+		displayLine("Hammurabi, we only have %s people to tend the crops!" % population)
+		canPlant = false
+	
+	if (grainNeeded > grain):
+		displayLine("Hammurabi, we only have %s bushels of grain in storage!" % grain)
+		canPlant = false
+	
+	if (acresToPlant > land):
+		displayLine("Hammurabi, we only have %s acres of land!" % land)
+		canPlant = false
+		
+	if (canPlant):
+		grain -= grainNeeded
+		acresPlantedThisYear = acresToPlant
+		displayLine("Very well.  We will plant %s acres of crops." % acresToPlant)
+		controlsContainer.setNextMode()
+	
+	
+func _on_feed_button_pressed():
+	var bushelsForFood:int = controlsContainer.quantity.value
+	
+	if (bushelsForFood <= grain):
+		grainFedToPeople = bushelsForFood
+		grain -= grainFedToPeople
+		displayLine("Very well.  We shall allocate %s bushels of grain for food." % grainFedToPeople)
+		controlsContainer.setPlantMode()
+	else:
+		displayLine("Hammurabi, we don't have that much grain.  There are only %s bushels in storage." % grain)
+
+
+func _on_next_year_button_pressed():
+	advanceYear()
+
+
+func _on_skip_button_pressed():
+	skipLandTrade()
---
+++ scripts/ui/controls.gd
@@ -0,0 +1,47 @@
+extends PanelContainer
+
+class_name ControlPanel
+
+@export var yearLabel:Label
+@export var quantity:SpinBox
+@export var buyButton:Button
+@export var sellButton:Button
+@export var skipButton:Button
+@export var plantButton:Button
+@export var feedButton:Button
+@export var nextButton:Button
+
+func hideAllButtons():
+	buyButton.hide()
+	sellButton.hide()
+	skipButton.hide()
+	plantButton.hide()
+	feedButton.hide()
+	nextButton.hide()
+	
+	quantity.value = 0
+
+
+func setBuySellMode():
+	hideAllButtons()
+	quantity.suffix = "acres"
+	buyButton.show()
+	sellButton.show()
+	skipButton.show()
+
+
+func setPlantMode():
+	hideAllButtons()
+	quantity.suffix = "acres"
+	plantButton.show()
+
+
+func setFeedMode():
+	hideAllButtons()
+	quantity.suffix = "bushels"
+	feedButton.show()
+
+
+func setNextMode():
+	hideAllButtons()
+	nextButton.show()
